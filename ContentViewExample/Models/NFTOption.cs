b1a0febfc11bb9abfb2122d44ea9e430ff11e3bb
Enmanuel Minaya <enmanuelminaya@gmail.com>
2021-09-09 18:34:09 -0400

    initial commit

---
+++ ContentViewExample/Models/NFTOption.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentViewExample.Models
+{
+    public class NFTOption
+    {
+        public NFTOption(string title, string image, string price)
+        {
+            Title = title;
+            Image = image;
+            Price = price;
+        }
+
+        public string Title { get; }
+        public string Image { get; }
+        public string Price { get; }
+    }
+}
---
+++ ContentViewExample/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ContentViewExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentViewExample.ViewModels
+{
+    public class MainViewModel: BaseViewModel
+    {
+
+       public NFTOption Token1{ get; }
+       public NFTOption Token2{get;}
+       public NFTOption Token3{get;}
+       public NFTOption Token4{get;}
+       public NFTOption Token5{get;}
+       public NFTOption Token6{get;}
+
+        public MainViewModel()
+        {
+           Token1 = new NFTOption("Punk", "a", "1000 ETH");
+           Token2 = new NFTOption("Punk", "b", "1500 ETH");
+           Token3 = new NFTOption("Punk", "c", "2000 ETH");
+           Token4 = new NFTOption("Punk", "d", "11000 ETH");
+           Token5 = new NFTOption("Punk", "e", "100 ETH");
+           Token6 = new NFTOption("Punk", "f", "500 ETH");
+        }
+    }
+}
